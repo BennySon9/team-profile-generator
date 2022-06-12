f051fa17c995d7c805da65d8eae91f84eb3a20c3
Brandon Benson <brandonbenson09@gmail.com>
2022-06-12 12:59:37 -0400

    fixed all testing errors

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,5 +1,5 @@
 const Intern = require("../lib/Intern");
-const intern = new Intern("John", "3", "john@fakemail.com", "2University");
+const intern = new Intern("John", "5", "john@fakemail.com", "2University");
 
 test("test if we can get the constructor values from intern object", () => {
   expect(intern.name).toBe("John");
@@ -9,21 +9,21 @@ test("test if we can get the constructor values from intern object", () => {
 });
 
 test("test if we get the name from the getName() method", () => {
-  expect(engineer.getName()).toBe("John");
+  expect(intern.getName()).toBe("John");
 });
 
 test("test if we get the id from the getId() method", () => {
-  expect(engineer.getId()).toBe("5");
+  expect(intern.getId()).toBe("5");
 });
 
 test("test if we get the email from the getEmail() method", () => {
-  expect(engineer.getEmail()).toBe("john@fakemail.com");
+  expect(intern.getEmail()).toBe("john@fakemail.com");
 });
 
 test("test if we get the school from the getSchool() method", () => {
-  expect(engineer.getSchool()).toBe("2University");
+  expect(intern.getSchool()).toBe("2University");
 });
 
 test("test if we get the role from the getRole() method", () => {
-  expect(engineer.getRole()).toBe("Intern");
+  expect(intern.getRole()).toBe("Intern");
 });
